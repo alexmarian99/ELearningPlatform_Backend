ca0c2e80611ee799c29b4fc9ec5b7dadff397521
IacobOliver <olialexander08@gmail.com>
2024-04-11 12:54:44 +0300

    fixed user and cors origins

--- src/main/java/cleancode/eLearningPlatform/auth/service/UserService.java
+++ src/main/java/cleancode/eLearningPlatform/auth/service/UserService.java
@@ -10,15 +10,12 @@ import cleancode.eLearningPlatform.modulesAndLessons.model.Status;
 import cleancode.eLearningPlatform.modulesAndLessons.model.Week;
 import cleancode.eLearningPlatform.modulesAndLessons.repository.LessonRepository;
 import cleancode.eLearningPlatform.modulesAndLessons.repository.WeekRepository;
-import cleancode.eLearningPlatform.specialKatas.model.Kata;
-import cleancode.eLearningPlatform.specialKatas.repository.KataRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
